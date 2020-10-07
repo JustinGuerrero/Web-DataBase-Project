461fb6ea6ecd1bbb4a48a9fcf121e750b6b2d75e
Justin Guerrero <justin.guerrero@ecat1.montana.edu>
2020-10-06 20:31:18 -0600

    Merge branch 'master' of https://github.com/msu/csci-440-fall2020
    # Conflicts:
    #	src/test/java/edu/montana/csci/csci440/homework/Homework2.java

--- src/main/java/edu/montana/csci/csci440/model/Model.java
+++ src/main/java/edu/montana/csci/csci440/model/Model.java
@@ -1,5 +1,10 @@
 package edu.montana.csci.csci440.model;
 
+import edu.montana.csci.csci440.util.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,31 +14,54 @@ public class Model {
 
     List<String> _errors = new LinkedList<>();
 
-    public boolean create() {
+    public boolean create()
+    {
         throw new UnsupportedOperationException("Needs to be implemented");
     }
 
-    public boolean update() {
+    public boolean update()
+    {
+        if (verify()) {
+            try (Connection conn = DB.connect();
+                 PreparedStatement stmt = conn.prepareStatement(
+                         "UPDATE employees SET FirstName=?, LastName=?, Email=? WHERE EmployeeId=?")) {
+                stmt.setString(1, this.getFirstName());
+                stmt.setString(2, this.getLastName());
+                stmt.setString(3, this.getEmail());
+                stmt.setLong(4, this.getEmployeeId());
+                stmt.executeUpdate();
+                return true;
+            } catch (SQLException sqlException) {
+                throw new RuntimeException(sqlException);
+            }
+        } else {
+            throw new UnsupportedOperationException("Error: that didn't work");
+            return false;
+    }
+    }
+
+    public void delete()
+    {
         throw new UnsupportedOperationException("Needs to be implemented");
     }
 
-    public void delete() {
+    public boolean verify()
+    {
         throw new UnsupportedOperationException("Needs to be implemented");
     }
 
-    public boolean verify() {
-        throw new UnsupportedOperationException("Needs to be implemented");
-    }
-
-    public void addError(String err) {
+    public void addError(String err)
+    {
         _errors.add(err);
     }
 
-    public List<String> getErrors() {
+    public List<String> getErrors()
+    {
         return _errors;
     }
 
-    public boolean hasErrors(){
+    public boolean hasErrors()
+    {
         return _errors.size() > 0;
     }
 
